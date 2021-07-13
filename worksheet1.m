c2d40a8b73fbf600696c243b4b9e0917f6ffbbcd
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2021-07-13 18:47:34 +0100

    Update worksheet1.m

--- worksheet1.m
+++ worksheet1.m
@@ -2,3 +2,44 @@
 
 x = [-7 7 3 -1 10 -4 4 -1 -1 -2];
 
+sprintf('x+3:')
+disp(x+3) 
+sprintf('x.^3:')
+disp(x.^3)
+sprintf('1./x:')
+disp(1./x)
+sprintf('x(3):')
+disp(x(3))
+sprintf('x(3:8):')
+disp(x(3:8)) 
+sprintf('x(3:end-2):')
+disp(x(3:end-2))
+sprintf('x(1:2:end):')
+disp(x(1:2:end)) 
+sprintf('x(end:-1:1):')
+disp(x(end:-1:1))  
+sprintf('x([2 8 4 6]):')
+disp(x([2 8 4 6]))
+sprintf('sum(x):')
+disp(sum(x)) 
+sprintf('cumsum(x):')
+disp(cumsum(x))  
+sprintf('prod(x):')
+disp(prod(x))
+sprintf('size(x):')
+disp(size(x)) 
+sprintf('length(x):')
+disp(length(x)) 
+sprintf('diag(x):')
+disp(diag(x))
+
+ 
+
+ 
+
+
+
+
+
+
+
